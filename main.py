7d13cc94b3f2dd6d083872ba27baf1c4d93583c9
rmurat <rony.murat@gmail.com>
2019-03-06 15:06:35 +0100

    rename files

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import json
 # import models
 # import trello_module
 import logging
-# from scrapping_modules import logic_immo
-from scrapping_modules import seloger
-# from scrapping_modules import leboncoin
+# from scrapping_modules import li
+from scrapping_modules import sel
+# from scrapping_modules import lbc
 from scrapping_modules import pap
 
 
@@ -18,44 +18,11 @@ script_path = os.path.abspath(__file__)
 os.chdir(os.path.dirname(script_path))
 
 # models.create_tables()
-"""
-SELOGER_CONF_LIST = [
-    "data/paris01.json",
-    "data/paris02.json",
-    "data/paris03.json",
-    "data/paris04.json",
-    "data/paris05.json",
-    "data/paris06.json",
-    "data/paris07.json",
-    "data/paris08.json",
-    "data/paris09.json",
-    "data/paris10.json",
-    "data/paris11.json",
-    "data/paris12.json",
-    "data/paris13.json",
-    "data/paris14.json",
-    "data/paris15.json",
-    "data/paris16.json",
-    "data/paris17.json",
-    "data/paris18.json",
-    "data/paris19.json",
-    "data/paris20.json",
-]
-
-logging.info("Retrieving from seloger")
-seloger.init_models()
-
-for conf in SELOGER_CONF_LIST:
-    with open(conf, encoding='utf-8') as parameters_data:
-        logging.info("read conf file: {}".format(conf))
-        parameters = json.load(parameters_data)
-        seloger.search(parameters)
-"""
 
 with open("data/sel.json", encoding='utf-8') as parameters_data:
     parameters = json.load(parameters_data)
-    seloger.init_models()
-    seloger.search(parameters)
+    sel.init_models()
+    sel.search(parameters)
 
 #with open("data/parameters.json", encoding='utf-8') as parameters_data:
 #    parameters = json.load(parameters_data)
