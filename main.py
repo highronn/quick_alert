7af74dbf5cd8d2311a1cdd1586ceab9eda9c3f50
rmurat <rony.murat@gmail.com>
2019-03-06 19:45:05 +0100

    se loger new version

--- main.py
+++ main.py
@@ -9,15 +9,13 @@ import logging
 # from scrapping_modules import li
 from scrapping_modules import sel
 # from scrapping_modules import lbc
-from scrapping_modules import pap
+# from scrapping_modules import pap
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 script_path = os.path.abspath(__file__)
 os.chdir(os.path.dirname(script_path))
-
-# models.create_tables()
 
 with open("data/sel.json", encoding='utf-8') as parameters_data:
     parameters = json.load(parameters_data)
--- scrapping_modules/sel.py
+++ scrapping_modules/sel.py
@@ -85,6 +85,15 @@ class AdSeLoger(Model):
         primary_key = False
 
 
+class AdSeLogerConf(Model):
+    class Meta:
+        database = quick_alert_db
+        db_table = 'sel_req_config'
+
+    id = CharField(unique=True, primary_key=True)
+    limit_date = DateTimeField(null=False)
+
+
 def convert_api_field_to_db_col(field_name):
     return field_name.replace('/', '_').lower()
 
@@ -96,14 +105,15 @@ def init_models():
             typ
         )
 
-    AdSeLoger.create_table()
+    AdSeLoger.create_table(safe=True)
+    AdSeLogerConf.create_table(safe=True)
 
 
 def search(params):
     AD_IDS = set()
 
     # ---------------------------
-    def read_ads(http_response):
+    def read_ads(http_response, limit_date):
         xml_root = ET.fromstring(http_response.text)
 
         for adNode in xml_root.findall('annonces/annonce'):
@@ -118,41 +128,74 @@ def search(params):
                 db_col = convert_api_field_to_db_col(field)
                 ad_fields[db_col] = field_value if field_value else None
 
-            #print("id: {} cp: {}".format(
-            #    ad_fields["idannonce"],
-            #    ad_fields["cp"]
-            #))
+            if limit_date and ad_fields["dtcreation"] <= limit_date:
+                return None
 
-            # try:
-            #     ad_model = AdSeLoger.create(**ad_fields)
-            #     # ad_model.save()
-            # except IntegrityError as error:
-            #     logging.info("ERROR: " + str(error))
-            # break
+            #logging.info("id: {} dt: {}".format(ad_fields["idannonce"], ad_fields["dtcreation"]))
+
+            try:
+                ad_model = AdSeLoger.create(**ad_fields)
+                # ad_model.save()
+            except IntegrityError as error:
+                logging.info("ERROR: " + str(error))
 
             id_annonce = ad_fields["idannonce"]
             if id_annonce in AD_IDS:
-                print("ERROR {} already received".format(id_annonce))
+                logging.error("annonce id [{}] already received".format(id_annonce))
             AD_IDS.add(id_annonce)
 
         return xml_root.findtext("pageSuivante")
 
     # ---------------------------
-    headers = {'user-agent': 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; D5803 Build/MOB30M.Z1)'}
-    response = requests.get(
-        "http://ws.seloger.com/search.xml",
-        params=params["request"], headers=headers
-    )
-    next_page = read_ads(response)
+    # config process
+    req_params = params['request']
 
     max_pages = params.get("max_pages", math.inf)
     max_pages = math.inf if max_pages <= 0 else max_pages
     page_count = 1
 
-    while next_page and page_count < max_pages:
-        print("read next page: {}".format(next_page))
-        response = requests.get(next_page, headers=headers)
-        next_page = read_ads(response)
-        page_count += 1
+    config_id = params["config_id"]
+    assert config_id, "no 'config_id' parameter set"
 
-    print("{} ads processed.".format(len(AD_IDS)))
+    only_new_ads = params.get("only_new_ads", False)
+    limit_date = None
+
+    # max_pages param has priority over only_new_ads
+    use_limit_date = only_new_ads and max_pages == math.inf
+    if use_limit_date:
+        if 'tri' not in req_params or req_params['tri'] != 'd_dt_crea':
+            logging.warning("force parameter 'tri' = 'd_dt_crea'")
+            req_params['tri'] = 'd_dt_crea'
+
+        try:
+            config = AdSeLogerConf.get(AdSeLogerConf.id == config_id)
+            limit_date = config.limit_date.strftime('%Y-%m-%dT%H:%M:%S')
+            logging.info("using limit date '{}'".format(limit_date))
+        except Exception:
+            logging.error("no config found for '{}'. no limit date will be used".format(config_id))
+    else:
+        limit_date = None
+
+    # request process
+    logging.info("process page {}".format(page_count))
+
+    headers = {'user-agent': 'Dalvik/2.1.0 (Linux; U; Android 6.0.1; D5803 Build/MOB30M.Z1)'}
+    response = requests.get(
+        "http://ws.seloger.com/search.xml",
+        params=req_params, headers=headers
+    )
+    next_page = read_ads(response, limit_date)
+
+    while next_page and page_count < max_pages:
+        page_count += 1
+        logging.info("process page {}".format(page_count))
+
+        response = requests.get(next_page, headers=headers)
+        next_page = read_ads(response, limit_date)
+
+    if use_limit_date:
+        new_limit_date = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        AdSeLogerConf.replace(id=config_id, limit_date=new_limit_date).execute()
+        logging.info("set '{}' config limit date to '{}'".format(config_id, new_limit_date))
+
+    logging.info("{} ads processed.".format(len(AD_IDS)))
